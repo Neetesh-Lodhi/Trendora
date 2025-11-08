260e2f1c4449185849576d7cbbf512ea01ec0fb0
neetesh <neeteshlodhi9555@email.com>
2025-11-08 23:37:45 +0530

    working on Admin Order Component

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -45,7 +45,13 @@ const placeOrderRazorpay = async (req, res) => {
 
 //All Orders data for Admin Panel
 const allOrders = async (req, res) => {
-          
+          try {
+             const orders = await orderModel.find({})
+             res.json({success:true,orders})
+          } catch (error) {
+             console.log(error)
+             res.json({success:false,message:error.message})
+          }
 }
 
 //User Orders Data for Frontend
@@ -63,7 +69,15 @@ const userOrders = async (req, res) => {
 
 //update Order Status
 const updateStatus = async (req, res) => {
-          
+      try {
+         const { orderId, status } = req.body
+         
+         await orderModel.findByIdAndUpdate(orderId, { status })
+         res.json({success:true,message:'Status Updated'})
+      } catch (error) {
+                    console.log(error);
+                    res.json({ success: false, message: error.message });
+      }    
 }
 
 export {
